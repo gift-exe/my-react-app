567d318e638a69ed610862d9b853c200ada6a8b7
Gift-py <giftabumere247@gmail.com>
2024-02-01 09:35:00 +0100

    ~ Sigh... In the end, I Could'nt escape java script lol

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+
+function MyButton() {
+  return(
+    <button>I am  a button</button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        <h1>Wellcome to react app</h1>
+        <MyButton/>
+    
     </div>
+    
+    
   );
 }
 
